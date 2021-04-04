f7b25bfa4c4a24d18066d4940eb5871115a6113c
Zero <node.zerocool@gmail.com>
2021-04-03 23:16:31 -0500

    get, post methods and routes

--- api.py
+++ api.py
@@ -11,11 +11,27 @@ STUDENTS = {
   '4': {'name': 'Kate', 'age': 22, 'spec': 'science'},
 }
 
+parser = reqparse.RequestParser()
+
+
 class StudentsList(Resource):
     def get(self):
         return STUDENTS
 
     def post(self):
+        parser.add_argument("name")
+        parser.add_argument("age")
+        parser.add_argument("spec")
+        args = parser.parse_args()
+        student_id = int(max(STUDENTS.key()))
+        student_id = "%i" % student_id
+        STUDENTS[student_id]= {
+            "name": args["name"],
+            "age": args["age"],
+            "spec": args["spec"],
+        }
+        return STUDENTS[student_id], 201
+
 
 api.add_resource(StudentsList, '/students/')
 
