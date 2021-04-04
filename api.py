247345de7574e5af5bfd47c4a5407f1df4324681
Zero <node.zerocool@gmail.com>
2021-04-03 23:12:49 -0500

    api.add_resource

--- api.py
+++ api.py
@@ -4,7 +4,20 @@ from flask_restful import Resource, Api, reqparse
 app = Flask(__name__)
 api = Api(app)
 
-Students = {}
+STUDENTS = {
+  '1': {'name': 'Mark', 'age': 23, 'spec': 'math'},
+  '2': {'name': 'Jane', 'age': 20, 'spec': 'biology'},
+  '3': {'name': 'Peter', 'age': 21, 'spec': 'history'},
+  '4': {'name': 'Kate', 'age': 22, 'spec': 'science'},
+}
+
+class StudentsList(Resource):
+    def get(self):
+        return STUDENTS
+
+    def post(self):
+
+api.add_resource(StudentsList, '/students/')
 
 if __name__== "__main__":
   app.run(debug=True)
