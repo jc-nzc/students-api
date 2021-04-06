452f490d781ea9359e22e655545be1c0381fe194
Zero <node.zerocool@gmail.com>
2021-04-05 22:59:49 -0500

    methods and logic

--- api.py
+++ api.py
@@ -32,13 +32,33 @@ class StudentsList(Resource):
         }
         return STUDENTS[student_id], 201
 
-class Student(Resource):
-  def get(self, student_id):
-  def put(self, student_id):
-  def delete(self, student_id):
-
 
 api.add_resource(StudentsList, '/students/')
+
+class Student(Resource):
+    def get(self, student_id):
+      if student_id not in STUDENTS:
+        return "Not found", 404
+      else:
+        return STUDENTS[student_id]
+
+    def put(self, student_id):
+      parser.add_argument("name")
+      parser.add_argument("age")
+      parser.add_argument("spec")
+      args = parser.parse_args()
+    if student_id not in STUDENTS:
+      return "Record not found", 404
+    else:
+      student = STUDENTS[student_id]
+      student["name"] = args["name"] if args["name"] is not None else student["name"]
+      student["age"] = args["age"] if args["age"] is not None else student["age"]
+      student["spec"] = args["spec"] if args["spec"] is not None else student["spec"]
+      return student, 200
+
+    def delete(self, student_id):
+
+api.add_resource(Student, '/students/<student_id>')
 
 if __name__== "__main__":
   app.run(debug=True)
